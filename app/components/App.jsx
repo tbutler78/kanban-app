7c97c11477d6f8d2c6d7cc3c198d6bc932a71b56
Tom <thomas.a.butler@gmail.com>
2018-01-18 12:26:30 -0700

    Setting up NoteActions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,67 +4,70 @@ import uuid from 'uuid';
 import Notes from './Notes';
 
 import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
 
 class App extends React.Component {
 
-  render() {
-      const {notes} = this.props;
+    render() {
+        const {notes} = this.props;
 
-    return (
-      <div>
+        return (
+            <div>
 
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes
-            notes={notes}
-            onNoteClick={this.activateNoteEdit}
-            onEdit={this.editNote}
-             onDelete={this.deleteNote} />
-      </div>
-    );
-  }
+                <button className="add-note" onClick={this.addNote}>+</button>
+                <Notes
+                    notes={notes}
+                    onNoteClick={this.activateNoteEdit}
+                    onEdit={this.editNote}
+                    onDelete={this.deleteNote}/>
+            </div>
+        );
+    }
 
-  addNote = () => {
-      this.setState({
-          notes: this.state.notes.concat([{
-              id: uuid.v4(),
-              task: 'New Task'
-          }])
-      });
-  }
+    addNote = () => {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New Task'
+            }])
+        });
+    }
 
-  deleteNote = (id, e) => {
-      // Avoid bubbling to edit
-      e.stopPropagation();
+    deleteNote = (id, e) => {
+        // Avoid bubbling to edit
+        e.stopPropagation();
 
-      this.setState({
-          notes: this.state.notes.filter(note => note.id !== id)
-      });
-  }
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== id)
+        });
+    }
 
-  activateNoteEdit = (id) => {
-      this.setState({
-          notes: this.state.notes.map(note => {
-              if (note.id === id){
-                  note.editing = true;
-              }
-              return note;
-          })
-      });
-  }
+    activateNoteEdit = (id) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = true;
+                }
+                return note;
+            })
+        });
+    }
 
-  editNote = (id, task) => {
-      this.setState({
-          notes: this.state.notes.map(note => {
-              if(note.id === id){
-                  note.editing = false;
-                  note.task = task;
-              }
-              return note;
-          })
-      });
-  }
+    editNote = (id, task) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = false;
+                    note.task = task;
+                }
+                return note;
+            })
+        });
+    }
 }
 
 export default connect(({notes}) => ({
     notes
-}))(App);
+}), {
+    NoteActions
+})(App);
