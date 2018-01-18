2de815778e7c586ea3a2f348b845ec246c1514d5
Tom <thomas.a.butler@gmail.com>
2018-01-18 10:57:47 -0700

    Connecting Editable with NOtes

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
 import Note from './Note';
+import Editable from './Editable';
 
 /**
  * {}'s allow us to mix JavaScript syntax within JSX.
  *  map returns a list of li elements for React to render.
  * key tells React which items have been changed/added/deleted
  */
-export default ({ notes, onDelete = () => {} }) => (
+export default ({
+  notes,
+  onNoteClick = () => {},
+  onEdit = () => {},
+  onDelete = () => {}
+}) => (
   <ul>
-    {notes.map(({id, task}) => 
+    {notes.map(({ id, editing, task }) => (
       <li key={id}>
-        <Note>
-            <span>{task}</span>
-            <button onClick={onDelete.bind(null, id)}>x</button>
-        </Note>  
+        <Note onClick={onNoteClick.bind(null, id)}>
+          <Editable
+            editing={editing}
+            value={task}
+            onEdit={onEdit.bind(null, id)} />
+          <button onClick={onDelete.bind(null, id)}>x</button>
+        </Note>
       </li>
-    )}
+    ))}
   </ul>
 );
