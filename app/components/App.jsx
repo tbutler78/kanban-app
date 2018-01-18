104352df631a8918d7cd7d303acda3453ae5fc17
Tom <thomas.a.butler@gmail.com>
2018-01-18 10:26:18 -0700

    pushing data to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+
+import uuid from 'uuid';
 import Notes from './Notes';
+
+const notes = [
+    {
+      id: uuid.v4(),
+      task: 'Learn React'
+    },
+    {
+      id: uuid.v4(),
+      task: 'Do laundry'
+    }
+  ];
 
 export default () => (
     <div>
         <button onClick={() => console.log('add note')}>+</button>
-        <Notes />
+        <Notes notes={notes} />
     </div>
 );
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import uuid from 'uuid';
-
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
 
 /**
  * {}'s allow us to mix JavaScript syntax within JSX.
  *  map returns a list of li elements for React to render.
  * key tells React which items have been changed/added/deleted
  */
-export default () => (
+export default ({notes}) => (
   <ul>{notes.map((note) => <li key={note.id}>{note.task}</li>)}</ul>
 )
