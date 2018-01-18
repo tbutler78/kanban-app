bbbc418660c81dee67b81963d3de78a105710025
Tom <thomas.a.butler@gmail.com>
2018-01-18 12:29:36 -0700

    Connecting NoteActions with NoteStore

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,7 +1,10 @@
 import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
 
 export default class NoteStore {
     constructor() {
+        this.bindActions(NoteActions);
+
         this.notes = [
             {
                 id: uuid.v4(),
@@ -12,5 +15,15 @@ export default class NoteStore {
                 task: 'Do laundry'
             }
         ];
+    }
+
+    create(note){
+        console.log('create note', note);
+    }
+    update(updatedNote){
+        console.log('update note', note');
+    }
+    delete(id){
+        console.log('delete note', id);
     }
 }
